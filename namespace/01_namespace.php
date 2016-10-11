9e7f012767ae9b94df3c61482ea519365aea5698
xiaoyezzf <martinzzfx@gmail.com>
2016-10-11 12:35:34 +0800

    new

---
+++ namespace/01_namespace.php
@@ -0,0 +1,24 @@
+<?php
+namespace Foo\Bar\subnamespace;
+header("Content-Type:text/html;charset=utf-8");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+const FOO = 1;
+function foo() {}
+class foo
+{
+  static function staticmethod() {}
+}
+?>
+</body>
+</html>
---
+++ namespace/02_namespace.php
@@ -0,0 +1,41 @@
+<?php
+namespace Foo\Bar;
+header("Content-Type:text/html;charset=utf-8");
+include_once './01_namespace.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+const FOO = 2;
+function foo() {}
+class foo
+{
+  static function staticmethod() {}
+}
+
+/* 非限定名称 */
+foo(); // 解析为 Foo\Bar\foo resolves to function Foo\Bar\foo
+foo::staticmethod(); // 解析为类 Foo\Bar\foo的静态方法staticmethod。resolves to class Foo\Bar\foo, method staticmethod
+echo FOO; // resolves to constant Foo\Bar\FOO   2
+
+/* 限定名称 */
+subnamespace\foo(); // 解析为函数 Foo\Bar\subnamespace\foo
+subnamespace\foo::staticmethod(); // 解析为类 Foo\Bar\subnamespace\foo,
+// 以及类的方法 staticmethod
+echo subnamespace\FOO; // 解析为常量 Foo\Bar\subnamespace\FOO    1
+
+/* 完全限定名称 */
+\Foo\Bar\foo(); // 解析为函数 Foo\Bar\foo
+\Foo\Bar\foo::staticmethod(); // 解析为类 Foo\Bar\foo, 以及类的方法 staticmethod
+echo \Foo\Bar\FOO; // 解析为常量 Foo\Bar\FOO   2
+?>
+</body>
+</html>
